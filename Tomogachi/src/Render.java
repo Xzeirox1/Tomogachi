f9643da5468596699180f2e78433e51c606e97e2
Xzeirox1 <159736848+Xzeirox1@users.noreply.github.com>
2024-10-15 15:00:59 +0200

    yes

--- Tomogachi/src/Render.java
+++ Tomogachi/src/Render.java
@@ -3,35 +3,35 @@ public class Render {
     public static void Menu()
     {
 
-        System.out.println("__________________________________________________________");
-        System.out.println("                                                          |");
-        System.out.println("                                      _______             |");
-        System.out.println("                                   .-'       `-.          |");
-        System.out.println("                                  /             \\         |");
-        System.out.println("   1) füttern                    |    O     O    |        |");
-        System.out.println("   2) spielen                    |       ^       |        |");
-        System.out.println("   3) baden                      |    \\_____/    |        |");
-        System.out.println("   4) gesundheit                 \\              /         |");
-        System.out.println("                                   `-._______.-'          |");
-        System.out.println("                                                          |");
-        System.out.println("__________________________________________________________|");
+        System.out.println(" __________________________________________________________");
+        System.out.println("|                                                          |");
+        System.out.println("|                                      _______             |");
+        System.out.println("|                                   .-'       `-.          |");
+        System.out.println("|                                  /             \\         |");
+        System.out.println("|   1) füttern                    |    O     O    |        |");
+        System.out.println("|   2) spielen                    |       ^       |        |");
+        System.out.println("|   3) baden                      |    \\_____/    |        |");
+        System.out.println("|   4) gesundheit                 \\              /         |");
+        System.out.println("|   5) shop                         `-._______.-'          |");
+        System.out.println("|                                                          |");
+        System.out.println("|__________________________________________________________|");
     }
     
 
     public static void Stats()
     {
         System.out.println("__________________________________________________________");
-        System.out.println("                                                         |");
-        System.out.println("   _________              _________                      |");
-        System.out.println("  |___"+Stats.swb+"___| Wasser     |___"+Stats.slp+"___| LP     _________    |");
-        System.out.println(" _________              _________          |__"+Stats.sdreck+"__|   |");
-        System.out.println("|___"+Stats.shb+"___| Hunger     |___"+Stats.sspass+"___| Spaß       Dreck      |");
-        System.out.println("                                                         |");
-        System.out.println("/////////////////////////////////////////////////////////|");
-        System.out.println("                                                         |");
-        System.out.println("                                                         |");
-        System.out.println("                                                         |");
-        System.out.println("_________________________________________________________|");
+        System.out.println("|                                                         |");
+        System.out.println("|   _________              _________                      |");
+        System.out.println("|  |___"+Stats.swb+"___| Wasser     |___"+Stats.slp+"___| LP     _________    |");
+        System.out.println("| _________              _________          |__"+Stats.sdreck+"__|   |");
+        System.out.println("||___"+Stats.shb+"___| Hunger     |___"+Stats.sspass+"___| Spaß       Dreck      |");
+        System.out.println("|                                                         |");
+        System.out.println("|/////////////////////////////////////////////////////////|");
+        System.out.println("|                                                         |");
+        System.out.println("|                                                         |");
+        System.out.println("|                                                         |");
+        System.out.println("|_________________________________________________________|");
     }
 
     public static void Essen(){
